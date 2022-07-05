3f9bf88ef29b019ca9c8f91ecfb25c3d13212970
herman <herman.ramaniuk@moorio.com>
2022-07-05 14:36:34 +0300

    * test 4

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 use App\Models\MenuItem;
@@ -24,76 +23,85 @@ class MenuController extends BaseController
     Sample response on GET /menu:
     ```json
     [
-        {
-            "id": 1,
-            "name": "All events",
-            "url": "/events",
-            "parent_id": null,
-            "created_at": "2021-04-27T15:35:15.000000Z",
-            "updated_at": "2021-04-27T15:35:15.000000Z",
-            "children": [
-                {
-                    "id": 2,
-                    "name": "Laracon",
-                    "url": "/events/laracon",
-                    "parent_id": 1,
-                    "created_at": "2021-04-27T15:35:15.000000Z",
-                    "updated_at": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 3,
-                            "name": "Illuminate your knowledge of the laravel code base",
-                            "url": "/events/laracon/workshops/illuminate",
-                            "parent_id": 2,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 4,
-                            "name": "The new Eloquent - load more with less",
-                            "url": "/events/laracon/workshops/eloquent",
-                            "parent_id": 2,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "name": "Reactcon",
-                    "url": "/events/reactcon",
-                    "parent_id": 1,
-                    "created_at": "2021-04-27T15:35:15.000000Z",
-                    "updated_at": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 6,
-                            "name": "#NoClass pure functional programming",
-                            "url": "/events/reactcon/workshops/noclass",
-                            "parent_id": 5,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 7,
-                            "name": "Navigating the function jungle",
-                            "url": "/events/reactcon/workshops/jungle",
-                            "parent_id": 5,
-                            "created_at": "2021-04-27T15:35:15.000000Z",
-                            "updated_at": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                }
-            ]
-        }
+    {
+    "id": 1,
+    "name": "All events",
+    "url": "/events",
+    "parent_id": null,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": [
+    {
+    "id": 2,
+    "name": "Laracon",
+    "url": "/events/laracon",
+    "parent_id": 1,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": [
+    {
+    "id": 3,
+    "name": "Illuminate your knowledge of the laravel code base",
+    "url": "/events/laracon/workshops/illuminate",
+    "parent_id": 2,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    },
+    {
+    "id": 4,
+    "name": "The new Eloquent - load more with less",
+    "url": "/events/laracon/workshops/eloquent",
+    "parent_id": 2,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    }
+    ]
+    },
+    {
+    "id": 5,
+    "name": "Reactcon",
+    "url": "/events/reactcon",
+    "parent_id": 1,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": [
+    {
+    "id": 6,
+    "name": "#NoClass pure functional programming",
+    "url": "/events/reactcon/workshops/noclass",
+    "parent_id": 5,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    },
+    {
+    "id": 7,
+    "name": "Navigating the function jungle",
+    "url": "/events/reactcon/workshops/jungle",
+    "parent_id": 5,
+    "created_at": "2021-04-27T15:35:15.000000Z",
+    "updated_at": "2021-04-27T15:35:15.000000Z",
+    "children": []
+    }
+    ]
+    }
+    ]
+    }
     ]
      */
 
-    public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+    public function getMenuItems()
+    {
+        $roots = MenuItem::where('parent_id', '=', '')->orWhereNull('parent_id')->get();
+        $i = 0
+        $menus = []
+        while ($roots[$i]) {
+            $menus = MenuItem::where('parent_id', '=', $roots[$i]['id'])->get();
+            ++$i
+            $menus->children = $menus
+        }
+        return $menus
     }
 }
