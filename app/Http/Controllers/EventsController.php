c10a788565e62f1f0318021c0cba25af009d0177
herman <herman.ramaniuk@moorio.com>
2022-07-05 13:45:58 +0300

    * webbee test2

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -101,7 +101,7 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        $comments = Event::all()->workshops;
     }
 
 
